86d9ba545035067b5315a2d9db350abf20c5021a
CF Bolz-Tereick <cfbolz@gmx.de>
2025-03-20 19:45:17 +0100

    make it possible to specify width/height of image

--- pyfidget/main.py
+++ pyfidget/main.py
@@ -3,15 +3,19 @@ from pyfidget.vm import render_image_naive, nested_list_to_ppm, Frame, Program
 from pyfidget.parse import parse
 
 def main(argv):
-    if len(argv) != 3:
-        print("Usage: %s <input.vm> <output.ppm>" % argv[0])
+    if len(argv) < 3:
+        print("Usage: %s <input.vm> <output.ppm> [length]" % argv[0])
         return 1
     with open(argv[1]) as f:
         code = f.read()
     operations = parse(code)
     frame = Frame(Program(operations))
     t1 = time.time()
-    data = render_image_naive(frame, 1000, 1000, -2, 2, -2, 2)
+    if len(argv) > 3:
+        length = int(argv[3])
+    else:
+        length = 1000
+    data = render_image_naive(frame, length, length, -2, 2, -2, 2)
     t2 = time.time()
     print("Time: %s" % (t2 - t1))
     nested_list_to_ppm(data, argv[2])
