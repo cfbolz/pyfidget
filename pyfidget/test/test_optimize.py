cd3de05793a0e3f154d0f8cf8dcb5ed9d2ef7eff
CF Bolz-Tereick <cfbolz@gmx.de>
2025-03-22 15:59:58 +0100

    property based tests for the optimizer

--- pyfidget/test/test_optimize.py
+++ pyfidget/test/test_optimize.py
@@ -1,7 +1,11 @@
 from __future__ import division, print_function
+import math
+
+from hypothesis import given, strategies, assume
 from pyfidget.optimize import optimize
 from pyfidget.parse import parse
-from pyfidget.vm import pretty_format, Program
+from pyfidget.vm import pretty_format, Program, DirectFrame
+from pyfidget.vm import Const, Operation, IntervalFrame, pretty_format
 
 def check_optimize(ops, minx=-1000, maxx=1000, miny=-1000, maxy=1000, minz=-1000, maxz=1000, expected=None):
     if isinstance(ops, str):
@@ -193,4 +197,93 @@ b abs x
 """
     check_optimize(ops, expected)
 
+# ____________________________________________________________
+# random test case generation
+
+regular_floats = strategies.floats(allow_nan=False, allow_infinity=False)
+
+all_operation_generators = []
+def opgen(func):
+    all_operation_generators.append(func)
+    return func
+
+@opgen
+def make_op0(data, operations):
+    func = data.draw(strategies.sampled_from(['var-x', 'var-y', 'var-z']))
+    return Operation(None, func, [])
+
+@opgen
+def make_const(data, operations):
+    return Const(None, data.draw(regular_floats))
+
+@opgen
+def make_op1(data, operations):
+    arg0 = data.draw(strategies.sampled_from(operations))
+    func = data.draw(strategies.sampled_from(['square', 'sqrt', 'neg', 'abs']))
+    return Operation(None, func, [arg0])
+
+@opgen
+def make_op2(data, operations):
+    arg0 = data.draw(strategies.sampled_from(operations))
+    arg1 = data.draw(strategies.sampled_from(operations))
+    func = data.draw(strategies.sampled_from(['add', 'sub', 'min', 'max', 'mul']))
+    return Operation(None, func, [arg0, arg1])
+
+@given(strategies.data())
+def test_random(data):
+    num_ops = data.draw(strategies.integers(1, 100))
+    num_final_ops = data.draw(strategies.integers(3, 10))
+    a = data.draw(regular_floats)
+    b = data.draw(regular_floats)
+    c = data.draw(regular_floats)
+    minx, x, maxx = sorted([a, b, c])
+
+    a = data.draw(regular_floats)
+    b = data.draw(regular_floats)
+    c = data.draw(regular_floats)
+    miny, y, maxy = sorted([a, b, c])
+
+    ops = []
+    for i in range(num_ops):
+        func = data.draw(strategies.sampled_from(all_operation_generators))
+        ops.append(func(data, ops))
+        ops[-1].name = '_%x' % i
+    prev_op = ops[-1]
+    for i in range(num_final_ops):
+        arg0 = data.draw(strategies.sampled_from(ops))
+        func = data.draw(strategies.sampled_from(['add', 'sub', 'min', 'max', 'mul']))
+        if data.draw(strategies.booleans()):
+            args = [arg0, prev_op]
+        else:
+            args = [prev_op, arg0]
+        prev_op = Operation("_fin%x" % i, func, args)
+        ops.append(prev_op)
+    program = Program(ops)
+    frame = DirectFrame(program)
+    resultops = optimize(program, minx, maxx, miny, maxy, 0.0, 0.0)
+    check_well_formed(resultops)
+    opt_frame = DirectFrame(Program(resultops))
+    try:
+        res = frame.run_floats(x, y, 0)
+    except Exception as e:
+        try:
+            res = opt_frame.run_floats(x, y, 0)
+        except Exception as e2:
+            assert type(e) is type(e2)
+        else:
+            pass # optimizer is allowed to remove exceptions
+    else:
+        if math.isnan(res):
+            return
+        reg_res = " " if res > 0 else "#"
+        res2 = opt_frame.run_floats(x, y, 0)
+        opt_res = " " if res2 > 0 else "#"
+        if not reg_res == opt_res:
+            print(x, y, res, res2, reg_res, opt_res)
+            formatted = pretty_format(ops)
+            print(formatted)
+            print("----")
+            formatted = pretty_format(resultops)
+            print(formatted)
+            assert 0
 
